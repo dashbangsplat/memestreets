1fa91ec2b0df0dacd32ec4cb9ef2517e82f327c0
dashbangsplat <travis@dashbangsplat.com>
2017-11-04 11:02:50 -0600

    working on boundary checking for player movement

--- game/stage/player/player.gd
+++ game/stage/player/player.gd
@@ -3,23 +3,37 @@ extends Node2D
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
-var body
+var STAGE
+var WIDTH
+var HEIGHT
+var VERTICAL_BOUNDARY_MIN_OFFSET
+var HORIZONTAL_BOUNDARY_MIN_OFFSET
 
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
+	STAGE = get_node("/root/Stage")
+	
+	var texture = get_node("Sprite").get_texture()
+	WIDTH = texture.get_width()
+	HEIGHT = texture.get_height()
+	VERTICAL_BOUNDARY_MIN_OFFSET = (HEIGHT / 2) - 10; # half the size of the texture (since it's centered) minus some value so it doesnt climb up the wall
+	HORIZONTAL_BOUNDARY_MIN_OFFSET = (WIDTH / 2); # half the size of the texture (since it's centered)
+	
 	set_process(true)
-	body = get_node("Body")
 	pass
 	
 func _process(delta):
 	var pos = get_pos()
-	if(Input.is_action_pressed("move_left")):
+	
+	# pos.x - HORIZONTAL_BOUNDARY_MIN_OFFSET becuase we want to check the limiot from the left of the texture
+	if(Input.is_action_pressed("move_left") && pos.x - HORIZONTAL_BOUNDARY_MIN_OFFSET > 0):
 		pos.x -= 1
 	elif(Input.is_action_pressed("move_right")):
 		pos.x += 1
 		
-	if(Input.is_action_pressed("move_up")):
+	# pos.y + VERTICAL_BOUNDARY_MIN_OFFSET becuase we want to check the limit from the bottom of the texture
+	if(Input.is_action_pressed("move_up") && pos.y + VERTICAL_BOUNDARY_MIN_OFFSET > STAGE.FLOOR_BOUNDARY):
 		pos.y -= 1
 	elif(Input.is_action_pressed("move_down")):
 		pos.y += 1
