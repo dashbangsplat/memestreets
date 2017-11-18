211c9b35eba1dae7d2ef2bb73c8181fcbfeb1bd6
dashbangsplat <travis@dashbangsplat.com>
2017-11-18 10:12:44 -0700

    fixed menu options in esc menu to move with camera and still animate (don't parent controls to non-control type nodes)

--- game/stage/hud/esc_menu/esc_menu_quit.gd
+++ game/stage/hud/esc_menu/esc_menu_quit.gd
@@ -3,31 +3,31 @@ extends Label
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
-var ANIM
-var MENU
+export (NodePath) var animNodePath
+export (NodePath) var escMenuNodePath
+var animNode
+var escMenuNode
 
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
-	ANIM = self.get_parent()
-	MENU = self.get_parent().get_parent().get_parent()
+	animNode = self.get_node(animNodePath)
+	escMenuNode = self.get_node(escMenuNodePath)
 
 func _on_Quit_mouse_enter():
-	ANIM.play("HighlightQuit")
-
+	animNode.play("HighlightQuit")
 
 func _on_Quit_mouse_exit():
-	ANIM.play_backwards("HighlightQuit")
-
+	animNode.play_backwards("HighlightQuit")
 
 func _on_Quit_input_event( ev ):
 	if (ev.type == InputEvent.MOUSE_BUTTON):
 		if (ev.button_index == 1):
 			self.get_tree().quit()
 
-
 func _on_EscMenu_visibility_changed():
-	if (MENU.is_visible()):
+	self.set("custom_colors/font_color", Color(1,1,1)) # reset color to white
+	if (escMenuNode.is_visible()):
 		self.show()
 	else:
 		self.hide()
